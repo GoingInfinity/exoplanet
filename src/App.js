30a6a3b87496b39d524e5c6c1536d3c209c6f77c
Jonathan Lee <jonathan.hyunho.lee@gmail.com>
2018-12-12 19:19:38 -0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xAxis: '',
+      yAxis: '',
+    };
+  }
+  
   render() {
     return (
       <div className="App">
