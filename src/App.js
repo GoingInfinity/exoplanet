f55ae762ca93d822fed2ecc61fed9a9241f0e64c
Jonathan Lee <jonathan.hyunho.lee@gmail.com>
2018-12-14 14:07:22 -0800

    Deleted unused code in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
-import AxisSelector from './components/axisSelector'
-import csvData from './assets/phl_hec_all_confirmed.csv'
-import './App.sass';
+import AxisSelector from './components/axisSelector';
 import PlotGraph from './components/plotGraph';
+
+import csvData from './assets/phl_hec_all_confirmed.csv';
+import './App.sass';
 
 class App extends Component {
   constructor(props) {
@@ -44,85 +45,6 @@ class App extends Component {
         ySelector: result.columns[11]
       })
     })
-  }
-
-  componentDidMount() {
-    // this.renderPlotGraph()
-    // this.renderHistogram('x')
-    // this.renderHistogram('y')
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // d3.selectAll("svg").remove()
-    if (prevState.xSelector !== this.state.xSelector || 
-      prevState.ySelector !== this.state.ySelector ) {
-      // this.renderPlotGraph()
-      // this.renderHistogram('x')
-      // this.renderHistogram('y')
-    } 
-  }
-
-  renderHistogram = (selector) => {
-    const { data, xSelector, ySelector } = this.state
-    let targetClass, axisSelection
-
-    // Histogram selector
-    if (selector === "x") {
-      targetClass = ".AxisSelector__xHistogram"
-      axisSelection = xSelector
-    } else
-    if (selector === "y") {
-      targetClass = ".AxisSelector__yHistogram"
-      axisSelection = ySelector
-    }
-    // CSS data for D3
-    const height = 100
-    const width = d3.select(targetClass).node().getBoundingClientRect().width
-    const margin = ({top: 20, right: 20, bottom: 30, left: 40})
-
-    // D3 template
-    const svg = d3.select(targetClass).append("svg").classed("AxisSelector__Svg", true).attr('width', width).attr('height', height)
-
-    // Filtered Data array for histogram
-    const dataArr = data.map(obj => obj[axisSelection])
-
-    // X-axis Linear Scale
-    const x = d3.scaleLinear()
-      .domain(d3.extent(dataArr)).nice()
-      .range([margin.left, width - margin.right])
-    
-    const bins = d3.histogram()
-      .domain(x.domain())
-      .thresholds(x.ticks(20))
-    (dataArr)
-
-    // Y-axis Linear Scale
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(bins, d => d.length)]).nice()
-      .range([height - margin.bottom, margin.top])
-    
-    // X-axis Positioning
-    const xAxis = g => g
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x).tickSizeOuter(0))
-    
-    // Y-axis Positioning
-    const yAxis = g => g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y))
-
-    // Append d3 data to DOM
-    svg.append("g")
-      .call(xAxis);
-    svg.append("g")
-        .attr("fill", "steelblue")
-      .selectAll("rect")
-      .data(bins)
-      .enter().append("rect")
-        .attr("x", d => x(d.x0) + 1)
-        .attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
-        .attr("y", d => y(d.length))
-        .attr("height", d => y(0) - y(d.length));
   }
 
   updateAxisSelection = (e, axis) => {
